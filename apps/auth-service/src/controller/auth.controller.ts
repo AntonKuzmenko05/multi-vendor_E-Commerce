c0cc181ebe1ca20a79f7d3f8fc84bcb74f1ed0c8
Anton Kuzmenko <kuzmenkoanton011@gmail.com>
2026-01-18 18:02:09 +0200

    Redis conf

--- apps/auth-service/src/controller/auth.controller.ts
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -1,6 +1,24 @@
 import {NextFunction, Request, Response} from "express";
+import {validateRegistrationData} from "../utils/auth.helper";
+import prisma from "../../../../packages/libs/prisma";
+import {ValidationError} from "../../../../packages/erorr-handler";
 
 //Register new user
-export const userRegistration = (req:Request, res:Response, next:NextFunction)=>{
+export const userRegistration = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    validateRegistrationData(req.body, "user")
+    const {name, email} = req.body;
 
+    const existingUser = await prisma.users.findUnique({where: email})
+
+    if(existingUser){
+        return next(new ValidationError("User already exists with this email!"))
+    }
+
+    await checkOtpRestrictions(email,next)
 }
+
+
--- apps/auth-service/src/utils/auth.helper.ts
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -1,4 +1,6 @@
 import {ValidationError} from "../../../../packages/erorr-handler";
+import {NextFunction} from "express";
+import * as crypto from "node:crypto";
 
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
@@ -9,6 +11,21 @@ export const validateRegistrationData = (data:any,userType:"user" | "seller")=>{
         !name || !email || !password || (userType === 'seller' && (!phone_number || !country))
       )
     {
-        return new ValidationError("Missing required fields");
+        throw new ValidationError("Missing required fields");
+    }
+
+    if(emailRegex.test(email)){
+      throw new ValidationError("Invalid email format")
     }
 }
+
+export const checkOtpRestrictions = (email:string, next:NextFunction) =>{
+
+}
+
+export const sendOtp = async(name:string, email:string, template:string)=.{
+    const otp = crypto.randomInt(1000, 9999).toString()
+
+    //
+}
+
