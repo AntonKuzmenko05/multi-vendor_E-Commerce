bc2d117b16914006317810131b4733f2e899387f
Anton Kuzmenko <kuzmenkoanton011@gmail.com>
2026-01-24 18:58:01 +0200

    api docs before config changes

--- apps/auth-service/src/controller/auth.controller.ts
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -15,11 +15,20 @@ export const userRegistration = async (
         validateRegistrationData(req.body, "user")
         const {name, email} = req.body;
 
+        //Має бути:
+        //
+        // const existingUser = await prisma.users.findUnique({
+        //   where: { email }
+        // })
+        //
+        //
+        // Інакше Prisma теж кине помилку — і вона теж прилетить у errorMiddleware.
         const existingUser = await prisma.users.findUnique({where: email})
 
         if (existingUser) {
             return next(new ValidationError("User already exists with this email!"))
         }
+
 
         await checkOtpRestrictions(email, next);
         await trackOtpRequests(email, next);
