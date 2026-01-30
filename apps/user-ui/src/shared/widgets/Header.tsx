fa64de828c12971b7af079642745671f2a99155b
Anton Kuzmenko <kuzmenkoanton011@gmail.com>
2026-01-30 17:09:14 +0200

    Header start

---
+++ apps/user-ui/src/shared/widgets/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from "next/link";
+
+const Header = () => {
+    return (
+        <div className='w-full bg-white'>
+            <div className='w-[80%] py-5 m-auto flex items-center justify-between'>
+                <div>
+                    <Link href={"/"}>
+                        <span className="text-2xl font-600 ">Eshop</span>
+                    </Link>
+                </div>
+                <div className='w-[50%] relative' >
+                    <input type="text" placeholder="Search for products"
+                           className='w-full px-4 font-Poppins font-medium border-[2.5px] border-[blue] outline-none h-[55px]'/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
