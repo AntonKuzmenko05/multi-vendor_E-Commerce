72d17853834a847537a639ce22e2305434524aa6
Anton Kuzmenko <kuzmenkoanton011@gmail.com>
2026-01-31 19:42:35 +0200

    HeaderBottom.tsx start.

--- apps/user-ui/src/shared/widgets/HeaderBottom.tsx
+++ apps/user-ui/src/shared/widgets/HeaderBottom.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+"use client";
+import React, {useEffect, useState} from 'react';
 
 const HeaderBottom = () => {
+    const [show, setShow] = useState(false)
+    const [isSticky, setIsSticky] = useState(false)
+
+    useEffect(()=>{
+        const handleScroll = () =>{
+            if (window.screenY > 100){
+                setIsSticky(true)
+            }else {
+                setIsSticky(false)
+            }
+            window.addEventListener('scroll', handleScroll)
+            return ()=> window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
     return (
-        <div>
-            HeaderBottom
+        <div className={`w-full transition-all duration-300 ${isSticky ? "fixed top-0 left-0  z-[100] bg-white shadow-lg"
+            : "relative"} `}>
+            <div className={`w-[80%] relative m-auto flex items-center justify-between ${
+                isSticky ? "pt-3" : "py-0"
+            }`}>
+                {/*All dropdowns*/}
+
+            </div>
         </div>
     );
 };
