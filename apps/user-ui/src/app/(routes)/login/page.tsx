6b885acadff40737004a9a771d48708238f38fc1
Anton Kuzmenko <kuzmenkoanton011@gmail.com>
2026-02-02 19:50:54 +0200

    Login page started

---
+++ apps/user-ui/src/app/(routes)/login/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+import React, {useState} from 'react';
+import {useRouter} from "next/navigation";
+import {useForm} from "react-hook-form";
+
+type FormData = {
+    email:string,
+    password:string,
+}
+
+
+const Login = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [serverError, setServerError] = useState<string | null>(null);
+    const [rememberMe, setRememberMe] = useState(false);
+
+    const router = useRouter()
+
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm<FormData>()
+
+    const onSubmit = (data:FormData) =>{
+
+    }
+
+    return (
+        <div className="w-full py-10 min-h-[85vh] bg-white">
+            <h1 className="text-4xl font-Poppins font-semibold text-black text-center">
+                Login
+            </h1>
+            <p className="text-center text-lg font-medium py-3 text-[#00000099]">
+                Home . Login
+            </p>
+        </div>
+    );
+};
+
+export default Login;
