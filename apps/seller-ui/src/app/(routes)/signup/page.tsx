b924a4f172c192bb64242816b583b0165af8f131
Anton Kuzmenko <kuzmenkoanton011@gmail.com>
2026-02-12 21:54:04 +0200

    sign up page for seller start

---
+++ apps/seller-ui/src/app/(routes)/signup/page.tsx
@@ -0,0 +1,284 @@
+'use client'
+import React, {useEffect, useRef, useState} from 'react';
+import {useRouter} from "next/navigation";
+import {useForm} from "react-hook-form";
+import Link from "next/link";
+import {Eye, EyeOff} from "lucide-react";
+import {useMutation} from "@tanstack/react-query";
+import axios, {AxiosError} from "axios";
+
+//TODO не відсилати лист якщо помилка і показувати помилку і показувати відлік скільик буде ще дійсний отп
+type FormData = {
+    email:string,
+    password:string,
+    name:string,
+}
+
+
+const Signup = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [showOtp, setShowOtp] = useState(false)
+    const [canResend, setCanResend] = useState(true);
+    const [timer, setTimer] = useState(60);
+    const [otp, setOtp] = useState(["","","",""])
+    const [userData, setUserData] = useState<FormData | null>(null);
+    const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+
+    const router = useRouter()
+
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm<FormData>();
+
+    const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+    // clear if unmount
+    useEffect(() => {
+        return () => {
+            if (timerRef.current) {
+                clearInterval(timerRef.current);
+            }
+        };
+    }, []);
+
+    const startResendTimer = () => {
+        // clear prev interval
+        if (timerRef.current) {
+            clearInterval(timerRef.current);
+        }
+
+        setTimer(60);
+        setCanResend(false);
+
+        timerRef.current = setInterval(() => {
+            setTimer((prev) => {
+                if (prev <= 1) {
+                    if (timerRef.current) {
+                        clearInterval(timerRef.current);
+                        timerRef.current = null;
+                    }
+                    setCanResend(true);
+                    return 0;
+                }
+                return prev - 1;
+            });
+        }, 1000);
+    };
+
+    const signupMutation = useMutation({
+        mutationFn: async (data:FormData) =>{
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URI}/api/user-registration`,
+                data
+            )
+            return response.data
+        },
+        onSuccess:(_,formData)=>{
+            setUserData(formData)
+            setShowOtp(true);
+            startResendTimer();
+        }
+    })
+
+    const verifyOtpMutation = useMutation({
+        mutationFn:async ()=>{
+            if(!userData) return
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URI}/api/verify-user`,
+                {
+                    ... userData,
+                    otp: otp.join("")
+                })
+            return response.data;
+        },
+        onSuccess: ()=>{
+            router.push("/login")
+        }
+    })
+
+    const onSubmit = (data:FormData) =>{
+        signupMutation.mutate(data)
+    }
+    const handleOtpChange = (index:number, value:string) =>{
+        if(!/^[0-9]?$/.test(value)) return;
+
+        const newOtp = [...otp];
+        newOtp[index] = value;
+        setOtp(newOtp);
+
+        if(value && index < inputRefs.current.length-1){
+            inputRefs.current[index+1]?.focus();
+        }
+    }
+
+    const handleOtpKeyDown = (index:number, e:React.KeyboardEvent<HTMLInputElement>)=>{
+        if (e.key === "Backspace" && !otp[index] && index>0){
+            inputRefs.current[index-1]?.focus()
+        }
+    }
+    const resendOtp = () => {
+        if (!canResend || !userData) return;
+
+        signupMutation.mutate(userData);
+    };
+
+    return (
+        <div className="w-full flex flex-col items-center pt-10 min-h-screen">
+            {/*Stepper*/}
+            <div className="relative flex items-center justify-between md:w-[50%] mb-8">
+                <div className="absolute top-[25%] left-0 w-[80%] md:w-[90%] h-1 bg-gray-300 -z-10"/>
+                {[1,2,3].map((step)=>(
+                    <div key={step}></div>
+                ))}
+            </div>
+            <div className="w-full py-10 min-h-screen bg-[#f1f1f1]">
+                <h1 className="text-4xl font-Poppins font-semibold text-black text-center">
+                    Sign Up
+                </h1>
+                <p className="text-center text-lg font-medium py-3 text-[#00000099]">
+                    Home . Sign Up
+                </p>
+                <div className="w-full flex justify-center">
+                    <div className="md:w-[480px] p-8 bg-white shadow rounded-lg">
+                        <h1 className="text-3xl font-semibold text-center mb-2">
+                            Sign Up to Eshop
+                        </h1>
+                        <p className="text-center text-gray-500 mb-4">
+                            Have an account? {" "}
+                            <Link href={"/login"} className="text-blue-500">
+                                Login
+                            </Link>
+                        </p>
+
+
+                        {!showOtp ? (
+                            <form onSubmit={handleSubmit(onSubmit)}>
+
+                                <label className="block text-gray-700 mb-1">Name</label>
+                                <input type="text"
+                                       placeholder="Wijab"
+                                       className="w-full p-2 border border-gray-300 outline-0 !rounded mb-1"
+                                       {...register("name", {
+                                           required: "Name is required",
+                                       })}/>
+                                {errors.name && (
+                                    <p className="text-red-500 text-sm">
+                                        {String(errors.name.message)}
+                                    </p>
+                                )}
+
+                                <label className="block text-gray-700 mb-1">Email</label>
+                                <input type="email"
+                                       placeholder="support@gmail.com"
+                                       className="w-full p-2 border border-gray-300 outline-0 !rounded mb-1"
+                                       {...register("email", {
+                                           required: "Email is required",
+                                           pattern: {
+                                               value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                               message: "Invalid email"
+                                           }
+                                       })}/>
+                                {errors.email && (
+                                    <p className="text-red-500 text-sm">
+                                        {String(errors.email.message)}
+                                    </p>
+                                )}
+
+                                <label className="block text-gray-700 mb-1">Password</label>
+                                <div className="relative">
+                                    <input type={passwordVisible ? "text" : "password"}
+                                           placeholder="Min 6 characters"
+                                           className="w-full p-2 border border-gray-300 outline-0 !rounded mb-1"
+                                           {...register("password", {
+                                               required: "Password is required",
+                                               minLength: {
+                                                   value: 6,
+                                                   message: "Password must be at least 6 "
+                                               }
+                                           })}
+                                    />
+
+                                    <button type="button" onClick={() => setPasswordVisible(!passwordVisible)}
+                                            className="absolute inset-y-0 right-3 flex items-center text-gray-400">
+                                        {passwordVisible ? <Eye/> : <EyeOff/>}
+                                    </button>
+                                </div>
+                                {errors.password && (
+                                    <p className="text-red-600 text-sm">
+                                        {String(errors.password.message)}
+                                    </p>
+                                )}
+
+
+                                <button
+                                    type="submit"
+                                    className="w-full text-lg cursor-pointer mt-4 bg-black text-white py-2 rounded-lg"
+                                    disabled={signupMutation.isPending}
+                                >
+                                    {signupMutation.isPending ? "Sign Up ...." : "Sign Up"}
+                                </button>
+                            </form>
+                        ) : (
+                            <div>
+                                //TODO paste OTP
+                                <h3 className="text-xl font-semibold text-center mb-4">Enter OTP</h3>
+                                <div className="flex justify-center gap-6 ">
+                                    {otp?.map((digit, index) => (
+                                        <input type="text" key={index} ref={(el) => {
+                                            if (el) inputRefs.current[index] = el;
+                                        }}
+                                               maxLength={1}
+                                               className="w-12 h-12 text-center border border-gray-300 outline-none !rounded"
+                                               value={digit}
+                                               onChange={e => handleOtpChange(index, e.target.value)}
+                                               onKeyDown={(e) => handleOtpKeyDown(index, e)}
+                                        />
+                                    ))}
+                                </div>
+                                <button
+                                    className="w-full mt-4 text-lg cursor-pointer bg-blue-500 text-white py-2 rounded-lg"
+                                    disabled={verifyOtpMutation.isPending}
+                                    onClick={() => verifyOtpMutation.mutate()}>
+                                    {verifyOtpMutation.isPending ? "Verifying..." : "Verify OTP"}
+                                </button>
+
+                                {/*
+                            {verifyOtpMutation.isError && (
+    <button onClick={() => {
+        setOtp(["","","",""]);
+        inputRefs.current[0]?.focus();
+    }}>
+        Try Again
+    </button>
+)}
+                            */}
+
+                                <p className="text-center text-sm mt-4">
+                                    {canResend ? (
+                                        <button onClick={resendOtp}
+                                                className="text-blue-500 cursor-pointer">
+                                            Resend OTP
+                                        </button>
+                                    ) : (
+                                        `Resend OTP in ${timer} s `
+                                    )}
+                                </p>
+                                {
+                                    verifyOtpMutation?.isError &&
+                                    verifyOtpMutation.error instanceof AxiosError && (
+                                        <p className="text-red-500 text-sm mt-2">
+                                            {verifyOtpMutation.error.response?.data?.message ||
+                                                verifyOtpMutation.error.message}
+                                        </p>
+                                    )
+                                }
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
